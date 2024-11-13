45ad5576183168017ea1bb707d6177e41984c87e
samsepi0x0 <samsepi0x0.gmail.com>
2024-11-13 17:51:21 +0530

    Refactored code so part 1 also solves part 2 Utilized Coordinate compression techniques (kinda) to achieve effiency

--- 2023/day11.py
+++ 2023/day11.py
@@ -1,46 +1,70 @@
-def part1(grid):
+def part1(grid, rows, cols):
+
     score = 0
     pairs = 0
+    n = 1000000
+
     for i in range(0, len(grid)):
         for j in range(0, len(grid[0])):
             if (grid[i][j] == '#'):
-            # found a hash, need to find distance to other galaxies.
-                #print(f"Found galaxy at {i, j}")
-                vertical = 0
-
+                vertical, horizontal = 0, 0
                 for x in range(j+1, len(grid[0])):
+                    if x in cols:
+                        horizontal += n 
+                    else:
+                        horizontal += 1 
                     if (grid[i][x] == '#'):
-                        #print(f"Found galaxy at {i, x}")
-                        score += (x - j)
-                
+                        pairs += 1 
+                        score = score + horizontal
+                        #print(f"Found source at {i, j} to destination at {i, x} with distance: 0i + {horizontal}j = {horizontal}")
+
                 for x in range(i+1, len(grid)):
-                    vertical += 1
+                    if x in rows:
+                        vertical += n 
+                    else:
+                        vertical += 1 
                     for y in range(0, len(grid[0])):
                         if (grid[x][y] == '#'):
                             pairs += 1
-                            #print(f"Found galaxy from {i, j} to {x, y}")
-                            #print(f"Vertical : {vertical} \tHorizontal: {abs(y-j)}")
-                            score += vertical + abs(y - j)
+                            horizontal = 0
+                            start = min(y, j)
+                            end = max(y, j)
+                            for a in range(start, end):
+                                if a in cols:
+                                    horizontal += n 
+                                else:
+                                    horizontal += 1 
+                            score += horizontal + vertical
+                            #print(f"Found source at {i, j} to destination at {x, y} with distance: {vertical}i + {horizontal}j = {vertical + horizontal}")
+
 
     print(f"Pairs: {pairs}\nScore: {score}")
 
 def main():
     lines = []
+    rows = []
+    cols = []
+    r = 0
     with open('input1.txt', 'r') as f:
         for line in f:
             lines.append(list(line.strip()))
             if not ('#' in line):
-                lines.append(list(line.strip()))
+                rows.append(r)
+            r += 1 
+
     result = [[lines[j][i] for j in range(len(lines))] for i in range(len(lines[0]))]
-    linestp = []
+    
     for i in range(len(result)):
         if not ('#' in result[i]):
-            linestp.append(result[i])
-        linestp.append(result[i])
+            cols.append(i)
     
-    #for line in linestp:
-    #    print(line)
-    part1(linestp)    
+    #for line in lines:
+        #print(line)
+    
+    #print(rows)
+    #print(cols)
+
+    part1(lines, rows, cols)    
 
 if __name__ == '__main__':
     main()
