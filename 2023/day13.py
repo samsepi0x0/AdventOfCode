362bd8c6a9e4053de8238e8e0b722788edb74132
samsepi0x0 <samsepi0x0@gmail.com>
2024-11-18 14:59:52 +0530

    Added part 2 for day 13

--- 2023/day13.py
+++ 2023/day13.py
@@ -10,6 +10,21 @@ def find_center(grid):
             return True, row
     return False, -1
 
+def find_center2(grid):
+    for row in range(1, len(grid)):
+        top = grid[:row][::-1]
+        bottom = grid[row:]
+
+        diff = 0
+        for x, y in zip(top, bottom):
+            for ch1, ch2 in zip(x, y):
+                if (ch1 != ch2):
+                    diff += 1
+
+        if (diff == 1):
+            return True, row
+    return False, -1
+
 def rotate(grid):
     grid_transpose = [""] * len(grid[0])
 
@@ -34,8 +49,25 @@ def part1(grids):
 
     print(f"Part 1: {score}")
 
+
+def part2(grids):
+    score = 0
+    for grid in grids:
+        found, center = find_center2(grid)
+
+        if (found):
+            score = score + (100 * center)
+            continue
+        
+        found, center = find_center2(rotate(grid))
+        if (found):
+            score = score + (center)
+
+    print(f"Part 2: {score}")
+
+
 def main():
-    file = open('input.txt', 'r')
+    file = open('input.txt.bak', 'r')
     lines = file.readlines()
     
     matrix = []
@@ -47,7 +79,7 @@ def main():
         else:
             matrix.append(line.strip())
     grid.append(matrix)
-    part1(grid)
+    part2(grid)
 
 
 main()
