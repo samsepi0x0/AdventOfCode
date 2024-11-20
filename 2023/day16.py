7495fb6cc0c47375b752c516d38a6468a86613f9
samsepi0x0 <samsepi0x0@gmail.com>
2024-11-20 18:46:06 +0530

    Part 1 using recursive backtracking

---
+++ 2023/day16.py
@@ -0,0 +1,79 @@
+import sys
+sys.setrecursionlimit(10000)
+
+def part1(grid):
+
+    visited = set() 
+
+    def f(dirn, r, c):
+        if (dirn, r, c) in visited:
+            return False
+        if (r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0])):
+            return False
+        visited.add((dirn, r, c))
+        match dirn:
+            case 'R':
+                if grid[r][c] == '.':
+                    return f(dirn, r, c+1)
+                if grid[r][c] == '-':
+                    return f(dirn, r, c+1)
+                if grid[r][c] == '/':
+                    return f('U', r-1, c)
+                if grid[r][c] == '\\':
+                    return f('D', r+1, c)
+                if grid[r][c] == '|':
+                    return f('U', r-1, c) or f('D', r+1, c)
+            case 'D':
+                match grid[r][c]:
+                    case '.':
+                        return f(dirn, r+1, c)
+                    case '|':
+                        return f(dirn, r+1, c)
+                    case '/':
+                        return f('L', r, c-1)
+                    case '\\':
+                        return f('R', r, c+1)
+                    case '-':
+                        return f('L', r, c-1) or f('R', r, c+1)
+            case 'L':
+                match grid[r][c]:
+                    case '.':
+                        return f(dirn, r, c-1)
+                    case '-':
+                        return f(dirn, r, c-1)
+                    case '/':
+                        return f('D', r+1, c)
+                    case '\\':
+                        return f('U', r-1, c)
+                    case '|':
+                        return f('D', r+1, c) or  f('U', r-1, c)
+            case 'U':
+                match grid[r][c]:
+                    case '.':
+                        return f(dirn, r-1, c)
+                    case '|':
+                        return f(dirn, r-1, c)
+                    case '/':
+                        return f('R', r, c+1)
+                    case '\\':
+                        return f('L', r, c-1)
+                    case '-':
+                        return f('R', r, c+1) or f('L', r, c-1)
+    f('R', 0, 0)
+    uniq_visited = set()
+    for _, r, c in visited:
+        uniq_visited.add((r, c))
+    print(f"Part 1: {len(uniq_visited)}")
+
+def main():
+    file = open("input.txt", "r")
+    lines = file.readlines()
+
+    grid = []
+    for line in lines:
+        grid.append(list(line.strip()))
+
+    part1(grid)
+
+if __name__ == "__main__":
+    main()
