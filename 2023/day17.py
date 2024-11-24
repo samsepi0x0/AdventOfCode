361eab1512c80dd15ce99b9bd2438a72a9f8dc9a
samsepi0x0 <samsepi0x0@gmail.com>
2024-11-25 02:01:00 +0530

    Added comments for clarity of part2

--- 2023/day17.py
+++ 2023/day17.py
@@ -7,7 +7,7 @@ def part1(grid):
     while pq:
         heat, r, c, dirx, diry, steps = heappop(pq)
 
-        if (r, c) == (len(grid)-1, len(grid[0])-1) and steps >= 4:
+        if (r, c) == (len(grid)-1, len(grid[0])-1) and steps >= 4: # "and steps >= 4" is for part 2
             print(f"Score: {heat}")
             break
 
@@ -15,13 +15,13 @@ def part1(grid):
             continue
         seen.add((r, c, dirx, diry, steps))
         
-        if steps < 10 and (dirx, diry) != (0, 0):
+        if steps < 10 and (dirx, diry) != (0, 0): # modified consecutive steps from 3 to 10
             x = r + dirx
             y = c + diry
             if (x >= 0 and x < len(grid)) and (y >= 0 and y < len(grid[0])):
                 heappush(pq, (heat + grid[x][y], x, y, dirx, diry, steps+1))
         
-        if (dirx, diry) == (0, 0) or steps >= 4:
+        if (dirx, diry) == (0, 0) or steps >= 4: # part 2 condition of minimum consecutive steps.
             for ndirx, ndiry in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
                 if (ndirx, ndiry) == (dirx, diry) or (ndirx, ndiry) == (-dirx, -diry):
                     continue
