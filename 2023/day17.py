fa875a936b98fde89b97f42f8b978f4538ceb7ca
samsepi0x0 <samsepi0x0@gmail.com>
2024-11-25 01:59:14 +0530

    Modified conditions to accomodate part 2 in part 1 itself.

--- 2023/day17.py
+++ 2023/day17.py
@@ -7,7 +7,7 @@ def part1(grid):
     while pq:
         heat, r, c, dirx, diry, steps = heappop(pq)
 
-        if (r, c) == (len(grid)-1, len(grid[0])-1):
+        if (r, c) == (len(grid)-1, len(grid[0])-1) and steps >= 4:
             print(f"Score: {heat}")
             break
 
@@ -15,22 +15,23 @@ def part1(grid):
             continue
         seen.add((r, c, dirx, diry, steps))
         
-        if steps < 3 and (dirx, diry) != (0, 0):
+        if steps < 10 and (dirx, diry) != (0, 0):
             x = r + dirx
             y = c + diry
             if (x >= 0 and x < len(grid)) and (y >= 0 and y < len(grid[0])):
                 heappush(pq, (heat + grid[x][y], x, y, dirx, diry, steps+1))
         
-        for ndirx, ndiry in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
-            if (ndirx, ndiry) == (dirx, diry) or (ndirx, ndiry) == (-dirx, -diry):
-                continue
-            x = r + ndirx
-            y = c + ndiry
-            if (x >= 0 and x < len(grid)) and (y >= 0 and y < len(grid[0])):
-                heappush(pq, (heat + grid[x][y], x, y, ndirx, ndiry, 1))
+        if (dirx, diry) == (0, 0) or steps >= 4:
+            for ndirx, ndiry in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
+                if (ndirx, ndiry) == (dirx, diry) or (ndirx, ndiry) == (-dirx, -diry):
+                    continue
+                x = r + ndirx
+                y = c + ndiry
+                if (x >= 0 and x < len(grid)) and (y >= 0 and y < len(grid[0])):
+                    heappush(pq, (heat + grid[x][y], x, y, ndirx, ndiry, 1))
         
 def main():
-    file = open("input.txt", "r")
+    file = open("input.txt.1", "r")
     lines = file.readlines()
 
     grid = []
