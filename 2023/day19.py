39dc08666c700ecacf3d52b5f552d99a9aa001c5
samsepi0x0 <samsepi0x0@gmail.com>
2024-11-26 23:43:57 +0530

    Wrote part2 from scratch because recursive approach is better that iterative when dealing with ranges.

--- 2023/day19.py
+++ 2023/day19.py
@@ -34,8 +34,67 @@ def part1(paths, values):
 
     print(f"Part 1: {score}")
 
-def main():
+def part2():
     file = open('input.txt.1', 'r')
+    lines = file.readlines()
+
+    paths = dict()
+
+    for line in lines:
+        line = line.strip()
+        if (len(line) == 0):
+            break
+        name, val = line[:-1].split("{")
+        vals = val.split(",")
+        paths[name] = ([], vals.pop())
+        for value in vals:
+            comp, dst = value.split(":")
+            key = comp[0]
+            cmp = comp[1]
+            n = int(comp[2:])
+            paths[name][0].append((key, cmp, n, dst))
+    
+    def count(ranges, path = "in"):
+        if path == 'R':
+            return 0
+        if path == 'A':
+            prod = 1
+            for i, j in ranges.values():
+                prod *= (j - i + 1)
+            return prod
+        
+        total = 0
+
+        flow, default = paths[path]
+
+        for key, cmp, n, dst in flow:
+            lo, hi = ranges[key]
+            if cmp == '<':
+                true = (lo, n-1)
+                false = (n, hi)
+            else:
+                true = (n+1, hi)
+                false = (lo, n)
+            if true[0] <= true[1]:
+                cp = dict(ranges)
+                cp[key] = true
+                total += count(cp, dst)
+            if false[0] <= false[1]:
+                ranges = dict(ranges)
+                ranges[key] = false
+            else:
+                break
+        else:
+            total += count(ranges, default)
+        
+        return total    
+
+
+    score = count({key: (1, 4000) for key in "xmas"})
+    print(score)
+
+def main():
+    file = open('input.txt', 'r')
     lines = file.readlines()
 
     paths = OrderedDict()
@@ -76,6 +135,7 @@ def main():
         values.append(value)
 
     part1(paths, values)
+    part2()
 
 if __name__ == "__main__":
     main()
