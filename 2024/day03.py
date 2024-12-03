6b0a4d5b308c6de2777b4d64196d0791911885ba
samsepi0x0 <samsepi0x0@gmail.com>
2024-12-03 14:30:11 +0530

    Day 03 Part 2, regex in python with OR operator(|) to match mul operation along with do and don't operation.

--- 2024/day03.py
+++ 2024/day03.py
@@ -13,11 +13,32 @@ def part1(lines):
     
     print(f"Part 1 : {score}")
 
+def part2(lines):
+    score = 0
+    enable = True
+    for line in lines:
+        line = line.strip()
+        matches = re.findall(r'mul\([0-9]+,[0-9]+\)|do\(\)|don\'t\(\)', line)
+
+        for match in matches:
+            if match == "do()":
+                enable = True
+            elif match == "don\'t()":
+                enable = False
+            
+            if (enable) and match[0] == 'm':
+                op, blob = match.split("(")
+                x, y = list(map(int, blob[:-1].split(",")))
+                score = score + (x*y)
+
+    print(f"Part 2 : {score}")
+
 def main():
     file = open('input.txt', 'r')
     lines = file.readlines()
 
     part1(lines)
+    part2(lines)
 
 if __name__ == "__main__":
     main()
