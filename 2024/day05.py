b74d97e9618b0e921d5ae2214aa1d2921e931ab4
samsepi0x0 <samsepi0x0@gmail.com>
2024-12-05 18:38:24 +0530

    Day 5 Part 1

---
+++ 2024/day05.py
@@ -0,0 +1,43 @@
+def part1(mapping, updates):
+    score = 0
+    
+    for update in updates:
+        accepted = True
+        n = len(update)
+
+        for i in range(1, n):
+            num = update[i]
+            for j in range(0, i):
+                if update[j] in mapping.keys() and num in mapping[update[j]]:
+                    accepted = False
+                    break
+            if not accepted:
+                break
+        if accepted:
+            score += update[n // 2]
+
+    print(f"Part 1 : {score}")
+
+def main():
+    file = open('input.txt', 'r')
+    lines = file.readlines()
+    mapping = dict()
+    seq = 0
+
+    for index, line in enumerate(lines):
+        if (line.strip() == ''):
+            seq = index
+            break
+        x, y = line.strip().split('|')
+        if int(y) not in mapping.keys():
+            mapping[int(y)] = list()
+        mapping[int(y)].append(int(x))
+    
+    updates = []
+    for index in range(seq+1, len(lines)):
+        updates.append(list(map(int, lines[index].strip().split(","))))
+
+    part1(mapping, updates)
+
+if __name__ == "__main__":
+    main()
