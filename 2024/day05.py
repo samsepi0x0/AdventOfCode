341b0b511372fe24175a1e0809973d95e17ce974
samsepi0x0 <samsepi0x0@gmail.com>
2024-12-05 18:44:54 +0530

    Day 5 Part 2

--- 2024/day05.py
+++ 2024/day05.py
@@ -18,6 +18,25 @@ def part1(mapping, updates):
 
     print(f"Part 1 : {score}")
 
+def part2(mapping, updates):
+    score = 0
+    
+    for update in updates:
+        accepted = True
+        n = len(update)
+
+        for i in range(1, n):
+            num = update[i]
+            for j in range(0, i):
+                if update[j] in mapping.keys() and num in mapping[update[j]]:
+                    accepted = False
+                    update[i], update[j] = update[j], update[i]
+
+        if not accepted:
+            score += update[n // 2]
+
+    print(f"Part 2 : {score}")
+
 def main():
     file = open('input.txt', 'r')
     lines = file.readlines()
@@ -38,6 +57,7 @@ def main():
         updates.append(list(map(int, lines[index].strip().split(","))))
 
     part1(mapping, updates)
+    part2(mapping, updates)
 
 if __name__ == "__main__":
     main()
