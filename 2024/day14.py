967b25a2e8f7762488cc05da5e982ce9302259ec
samsepi0x0 <samsepi0x0@gmail.com>
2024-12-15 16:06:13 +0530

    Day 14 Part 2, Christmas tree at minimum Security Factor score time

--- 2024/day14.py
+++ 2024/day14.py
@@ -2,6 +2,7 @@ file = open("input.txt", "r")
 lines = file.readlines()
 
 M, N = 101, 103
+# M, N = 11, 7
 robots = {}
 
 for id, line in enumerate(lines):
@@ -11,35 +12,103 @@ for id, line in enumerate(lines):
 
     robots[id] = [(int(px), int(py)), (int(vx), int(vy))]
 
-
-for robot, values in robots.items():
-    pos, vel = values
-    px, py = pos
-    vx, vy = vel
-    
-    # print(px, py, vx, vy)
-    
-    Px = (px + vx*100) % M
-    Py = (py + vy*100) % N
-
-    robots[robot] = ((Px, Py), (vx, vy))
+def part1():
+    quads = [0, 0, 0, 0]
+    for robot, values in robots.items():
+        pos, vel = values
+        px, py = pos
+        vx, vy = vel
         
-quads = [0, 0, 0, 0]
-for robot, values in robots.items():
-    pos, vel = values
-    px, py = pos
+        # print(px, py, vx, vy)
+        
+        px = (px + vx*100) % M
+        py = (py + vy*100) % N
 
-    if (px < M // 2 and py < N // 2):
-        quads[0] += 1
-    elif (px > M //2 and py < N // 2):
-        quads[1] += 1
-    elif (px < M // 2 and py > N // 2):
-        quads[2] += 1
-    elif (px > M // 2 and py > N // 2):
-        quads[3] += 1
+        if (px < M // 2 and py < N // 2):
+            quads[0] += 1
+        elif (px > M //2 and py < N // 2):
+            quads[1] += 1
+        elif (px < M // 2 and py > N // 2):
+            quads[2] += 1
+        elif (px > M // 2 and py > N // 2):
+            quads[3] += 1
 
-score = 1
-for val in quads:
-    score *= val
+    score = 1
+    for val in quads:
+        score *= val
 
-print(f"Part 1: {score}")
+    print(f"Part 1: {score}")
+
+def part2():
+    minScore = float('inf')
+    bestTime = None
+    for time in range(M*N):
+        quads = [0, 0, 0, 0]
+        for robot, values in robots.items():
+            pos, vel = values
+            px, py = pos
+            vx, vy = vel
+            Px = (px + vx*time) % M
+            Py = (py + vy*time) % N
+
+            if (Px < M // 2 and Py < N // 2):
+                quads[0] += 1
+            elif (Px > M //2 and Py < N // 2):
+                quads[1] += 1
+            elif (Px < M // 2 and Py > N // 2):
+                quads[2] += 1
+            elif (Px > M // 2 and Py > N // 2):
+                quads[3] += 1
+        score = quads[0]*quads[1]*quads[2]*quads[3]
+        if (score < minScore):
+            minScore = score
+            bestTime = time
+    
+    print(f"Part 2 : {bestTime}")
+
+    for robot, values in robots.items():
+        pos, vel = values
+        px, py = pos
+        vx, vy = vel
+
+        # print(px, py, vx, vy)
+
+        Px = (px + vx*bestTime) % M
+        Py = (py + vy*bestTime) % N
+
+        robots[robot] = ((Px, Py), (vx, vy))
+
+    grid = [['-' for _ in range(M)] for i in range(N)]
+
+    for robot, values in robots.items():
+        pos, vel = values
+        px, py = pos
+        grid[py][px] = "#"
+
+    from PIL import Image, ImageDraw
+    cell_size = 10
+    cell_border = 0
+
+    # Create a blank canvas
+    img = Image.new(
+        "RGBA",
+        (M * cell_size, N * cell_size),
+        "black"
+    )
+    draw = ImageDraw.Draw(img)
+
+    for i, row in enumerate(grid):
+        for j, col in enumerate(row):
+            if col == '#':
+                fill = (255, 255, 255)
+            else:
+                fill = (0, 0, 0)
+            draw.rectangle(
+                ([(j * cell_size + cell_border, i * cell_size + cell_border),
+                    ((j + 1) * cell_size - cell_border, (i + 1) * cell_size - cell_border)]),
+                fill=fill
+            )
+    img.save("Easter.png")
+
+part1()
+part2()
