477742fa46e3ef5c28624cd9cf75180c5db044a8
samsepi0x0 <samsepi0x0@gmail.com>
2024-12-18 14:13:56 +0530

    Day 18 Part 2

--- 2024/day18.py
+++ 2024/day18.py
@@ -15,23 +15,33 @@ for i in range(bytes):
     x, y = int(line[0]), int(line[1])
     grid[y][x] = '#'
 
-for line in grid:
-    print("".join(line))
+def solve(grid):
+    q = deque([(0, 0, 0)])
+    seen = {(0, 0)}
 
-q = deque([(0, 0, 0)])
-seen = {(0, 0)}
+    while q:
+        r, c, cost = q.popleft()
+        if (r, c) == (N-1, N-1):
+            return cost   
+        for nr, nc in [(r + 1, c), (r - 1, c),  (r, c + 1), (r, c - 1)]:
+            if nr < 0 or nr >= N or nc < 0 or nc >= N:
+                continue
+            if grid[nr][nc] == '#':
+                continue
+            if (nr, nc) in seen:
+                continue 
+            seen.add((nr, nc))
+            q.append((nr, nc, cost+1))
+    return -1
 
-while q:
-    r, c, cost = q.popleft()
-    if (r, c) == (N-1, N-1):
-        print(f"Part 1: {cost}")
-        break   
-    for nr, nc in [(r + 1, c), (r - 1, c),  (r, c + 1), (r, c - 1)]:
-        if nr < 0 or nr >= N or nc < 0 or nc >= N:
-            continue
-        if grid[nr][nc] == '#':
-            continue
-        if (nr, nc) in seen:
-            continue 
-        seen.add((nr, nc))
-        q.append((nr, nc, cost+1))
+print(f"Part 1 : {solve(grid)}")
+
+grid = [['.' for _ in range(N)] for _ in range(N)]
+
+for i in range(len(lines)):
+    line = lines[i].strip().split(",")
+    x, y = int(line[0]), int(line[1])
+    grid[y][x] = '#'
+    if (solve(grid) == -1):
+        print(f"Part 2 : {x},{y}")
+        break
