ec813f4a492183e535acb458820b275fb299c035
samsepi0x0 <samsepi0x0@gmail.com>
2024-12-19 11:37:40 +0530

    Day 19 Part 1, Recursion with LRU_CACHE

--- 2024/day19.py
+++ 2024/day19.py
@@ -13,20 +13,16 @@ for line in lines[2:]:
 @lru_cache
 def check(pattern):
     if (pattern == ""):
-        return 1
-    flag = 0
+        return True
+    flag = False
     for towel in towels:
         if pattern.startswith(towel):
-            flag += check(pattern[len(towel):])
+            flag = flag or check(pattern[len(towel):])
     return flag
 
 score = 0
-score2 = 0
 for pattern in patterns:
-    val = check(pattern)
-    score2 += val
-    if (val > 0):
+    if check(pattern):
         score += 1
 
 print(f"Part 1 : {score}")
-print(f"Part 2 : {score2}")
