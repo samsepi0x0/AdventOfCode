ac12f91b3c670500675cb74d7c09f0942d004ba1
samsepi0x0 <samsepi0x0@gmail.com>
2024-12-20 16:49:43 +0530

    Day 20 Part 2

--- 2024/day20.py
+++ 2024/day20.py
@@ -1,79 +1,66 @@
 from collections import deque
-from tqdm import tqdm
 
-file = open("input.txt", 'r')
+file = open("input.txt", "r")
 lines = file.readlines()
 
-grid = [list(line.strip()) for line in lines]
+grid = []
 
+for line in lines:
+    grid.append(list(line.strip()))
+
+M, N = len(grid), len(grid[0])
 sr, sc = 0, 0
-er, ec = 0, 0
+
 for row, line in enumerate(grid):
     for col, ch in enumerate(line):
-        if ch == 'S':
+        if (ch == 'S'):
             sr, sc = row, col
-        if ch == 'E':
-            er, ec = row, col
 
-def search(grid):
-    global sr, sc, er, ec
-    m, n = len(grid), len(grid[0])
+dists = [[-1] * N for _ in range(M)]
+dists[sr][sc] = 0
 
-    q = deque([(sr, sc, 0)])
-    seen = {(sr, sc)}
-    track = {(sr, sc): None}
-    Cost = 0
+while grid[sr][sc] != 'E':
+    for nr, nc in [(sr+1, sc), (sr-1, sc), (sr, sc+1), (sr, sc-1)]:
+        if nr < 0 or nr >= M or nc < 0 or nc >= N:
+            continue
+        if grid[nr][nc] == '#':
+            continue
+        if dists[nr][nc] != -1:
+            continue
+        dists[nr][nc] = dists[sr][sc] + 1
+        sr = nr
+        sc = nc
 
-    while (q):
-        r, c, cost = q.popleft()
-        if grid[r][c] == 'E':
-            Cost = cost
-            break
-        for nr, nc in [(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)]:
-            if (nr < 0 or nr >= m or nc < 0 or nc >= n):
-                continue
-            if (nr, nc) in seen:
+
+score = 0
+for r in range(M):
+    for c in range(N):
+        if (grid[r][c] == '#'):
+            continue
+        for nr, nc in [(r + 2, c), (r + 1, c + 1), (r, c + 2), (r - 1, c + 1)]:
+            if nr < 0 or nr >= M or nc < 0 or nc >= N:
                 continue
             if grid[nr][nc] == '#':
                 continue
-            q.append((nr, nc, cost + 1))
-            seen.add((nr, nc))
-            track[(nr, nc)] = (r, c)
+            if abs(dists[r][c] - dists[nr][nc]) >= 102:
+                score += 1
 
-    path = set()
+print(f"Part 1 : {score}")
 
-    while track[(er, ec)] != None:
-        path.add(track[(er, ec)])
-        er, ec = track[(er, ec)]
-    
-    return Cost, path 
-
-initialCost, initialPath = search(grid)
-print(f"Initial cost: {initialCost}")
-walls = set()
-costChange = dict()
 score = 0
-
-for cell in tqdm(initialPath):
-    r, c = cell
-    for nr, nc in [(r+1, c), (r-1, c), (r, c+1), (r, c-1)]:
-        if (nr, nc) in initialPath:
+for r in range(M):
+    for c in range(N):
+        if (grid[r][c] == '#'):
             continue
-        if (nr, nc) in walls:
-            continue
-        if grid[nr][nc] != '#':
-            continue
-        walls.add((nr, nc))
-        grid[nr][nc] = '.'
-        new_cost, new_path = search(grid)
-        grid[nr][nc] = '#'
-        diff = initialCost - new_cost
-        if not (diff in costChange.keys()):
-            costChange[diff] = 0
-        costChange[diff] += 1
+        for rad in range(2, 21):
+            for dr in range(0, rad+1):
+                dc = rad - dr
+                for nr, nc in {(r + dr, c + dc), (r+dr, c-dc), (r-dr, c+dc), (r-dr, c-dc)}:
+                    if nr < 0 or nr >= M or nc < 0 or nc >= N:
+                        continue
+                    if grid[nr][nc] == '#':
+                        continue
+                    if dists[r][c] - dists[nr][nc] >= 100+rad:
+                        score += 1
 
-for key in sorted(costChange.keys()):
-    if key >= 100:
-        score += costChange[key]
-
-print(f"Part 1: {score}")
+print(f"Part 2 : {score}")
