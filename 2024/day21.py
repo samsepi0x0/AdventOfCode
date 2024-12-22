f39a07b8ab48a81ba018f3caa9902a054a1553ad
samsepi0x0 <samsepi0x0@gmail.com>
2024-12-22 11:13:12 +0530

    Day 21 Part 2

--- 2024/day21.py
+++ 2024/day21.py
@@ -1,10 +1,11 @@
 from collections import deque
 from itertools import product
+from functools import cache
 
 file = open("input.txt", "r")
 lines = file.readlines()
 
-def solve(string, grid):
+def compute(grid):
     pos = {}
     for r in range(len(grid)):
         for c in range(len(grid[0])):
@@ -38,6 +39,9 @@ def solve(string, grid):
                     continue
                 break
             seq[(x, y)] = possible
+    return seq
+
+def solve(string, seq):
     options = [seq[(x, y)] for x,y in zip("A"+string, string)]
     return ["".join(x) for x in product(*options)]
 
@@ -45,16 +49,19 @@ def solve(string, grid):
 keypad = [["7","8","9"],["4", "5", "6"], ["1", "2", "3"], [None, "0", "A"]]
 dirpad = [[None, "^", "A"], ["<", "v", ">"]]
 
+keypad_seq = compute(keypad)
+dirpad_seq = compute(dirpad)
+dirpad_len = {key : len(value[0]) for key, value in dirpad_seq.items()}
 score = 0
 
 for line in lines:
     line = line.strip()
-    level1 = solve(line, keypad)
+    level1 = solve(line, keypad_seq)
     l2 = level1
     for _ in range(2):
         level2 = []
         for seq in level1:
-            level2 += solve(seq, dirpad)
+            level2 += solve(seq, dirpad_seq)
         minlen = min(map(len, level2))
         l2 = [seq for seq in level2 if len(seq) == minlen]
         level1 = l2
@@ -63,3 +70,23 @@ for line in lines:
     score += (length * value)
 
 print(f"Part 1 : {score}")
+
+@cache
+def compute2(seq, depth=25):
+    if depth == 1:
+        return sum([dirpad_len[(x, y)] for x, y in zip("A"+seq, seq)])
+    length = 0
+    for x,y in zip("A"+seq, seq):
+        length += min(compute2(s, depth-1) for s in dirpad_seq[(x, y)])
+    return length
+
+score = 0
+
+for line in lines:
+    line = line.strip()
+    level1 = solve(line, keypad_seq)
+    length = min(map(compute2, level1))
+    value = int(line[:-1], 10)
+    score += (length * value)
+
+print(f"Part 2 : {score}")
