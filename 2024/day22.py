7466c5a8cd09c0e1f3b393b5b2b1e11cfd51bdc6
samsepi0x0 <samsepi0x0@gmail.com>
2024-12-22 14:32:32 +0530

    Day 22 Part 2 (Unoptimized)

--- 2024/day22.py
+++ 2024/day22.py
@@ -13,6 +13,7 @@ def compute(n):
     n %= 16777216
     return n
 
+
 score = 0
 for line in lines:
     num = int(line.strip())
@@ -21,3 +22,30 @@ for line in lines:
     score += num
 
 print(f"Part 1: {score}")
+
+totalChanges = {}
+maxBuy = 0
+for line in lines:
+    num = int(line.strip())
+    diff = []
+    changes = {}
+    for i in range(1, 2000):
+        num2 = compute(num)
+        a, b = num % 10, num2 % 10
+        diff.append(b - a)
+        if (len(diff) > 4):
+            diff = diff[1:]
+        if (len(diff) == 4):
+            st = (diff[0], diff[1], diff[2], diff[3])
+            if st not in changes.keys():
+                changes[st] = b
+        num = num2
+    for key, val in changes.items():
+        if key not in totalChanges.keys():
+            totalChanges[key] = list()
+        totalChanges[key].append(val)
+
+for key, value in totalChanges.items():
+    maxBuy = max(maxBuy, sum(value))
+
+print(f"Part 2: {maxBuy}")
