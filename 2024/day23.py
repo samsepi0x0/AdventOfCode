cbb56e7d1bfc49e7750297392e0134e305d003f7
samsepi0x0 <samsepi0x0@gmail.com>
2024-12-23 14:35:03 +0530

    Day 23 Part 2, Recursive approach

--- 2024/day23.py
+++ 2024/day23.py
@@ -6,11 +6,11 @@ adj = dict()
 for line in lines:
     line = line.strip().split("-")
     if (line[0] not in adj.keys()):
-        adj[line[0]] = []
+        adj[line[0]] = set()
     if (line[1] not in adj.keys()):
-        adj[line[1]] = []
-    adj[line[0]].append(line[1])
-    adj[line[1]].append(line[0])
+        adj[line[1]] = set()
+    adj[line[0]].add(line[1])
+    adj[line[1]].add(line[0])
 
 threepairs = set()
 
@@ -29,3 +29,26 @@ for x, y, z in threepairs:
         score += 1
 
 print(f"Part 1 : {score}")
+
+sets = set()
+
+def search(node, req):
+    key = tuple(sorted(req))
+    if key in sets:
+        return
+    sets.add(key)
+    for neighbor in adj[node]:
+        if neighbor in req:
+            continue
+        if not all(neighbor in adj[query] for query in req):
+            continue
+        copy = set(req)
+        copy.add(neighbor)
+        search(neighbor, copy)
+
+for node in adj.keys():
+    search(node, {node})
+
+password = max(sets, key=len)
+
+print(f"Part 2 : ", ",".join(sorted(list(password))))
