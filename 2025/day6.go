6b4a7c18030abceef2d7fce3f9fdf45bcba64796
samsepi0x0 <64140687+samsepi0x0@users.noreply.github.com>
2026-08-11 17:20:18 +0530

    Add files via upload

---
+++ 2025/day6.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"strconv"
+	"bufio"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Enter Filename.")
+		return
+	}
+	filename := os.Args[1]
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("Error loading the file")
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var lines [][]int64
+	var operations []string
+	for scanner.Scan() {
+		line := scanner.Text()
+		numbers := strings.Split(line, " ")
+
+		if numbers[0] == "*" || numbers[0] == "+" {
+			operations = numbers
+			continue
+		}
+
+		var newline []int64
+		for i := 0; i < len(numbers); i++ {
+			number, _ := strconv.Atoi(numbers[i])
+			newline = append(newline, int64(number))
+		}
+		lines = append(lines, newline)
+		
+	}
+	var result1 int64
+
+	for i := 0; i < len(operations); i++ {
+		temp := int64(0)
+		if operations[i] == "*" {
+			temp = int64(1)
+		}
+		for j := 0; j < len(lines); j++ {
+			if operations[i] == "+" {
+				temp = temp + lines[j][i]
+			} else {
+				temp = temp * lines[j][i]
+			}
+		}
+		result1 += temp
+	}
+	fmt.Println("Day 06 Part 01 :", result1)
+}
