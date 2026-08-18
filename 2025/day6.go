ffda36d23e04a6f75cdbb94f506b84d6203434f7
samsepi0x0 <64140687+samsepi0x0@users.noreply.github.com>
2026-08-19 01:51:25 +0530

    Update day6.go
    Working code for part 1, with minimal garbage from in-complete part 2

--- 2025/day6.go
+++ 2025/day6.go
@@ -21,26 +21,44 @@ func main() {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
+
 	var lines [][]int64
 	var operations []string
+	
 	for scanner.Scan() {
 		line := scanner.Text()
 		numbers := strings.Split(line, " ")
 
 		if numbers[0] == "*" || numbers[0] == "+" {
-			operations = numbers
+			for i := 0; i < len(numbers); i++ {
+				if numbers[i] == "+" || numbers[i] == "*" {
+					operations = append(operations, numbers[i])
+				}
+			}
 			continue
 		}
 
 		var newline []int64
 		for i := 0; i < len(numbers); i++ {
+			if numbers[i] == "" {
+				continue
+			}
 			number, _ := strconv.Atoi(numbers[i])
+			number2 := 0
+			numberCopy := number
+
+			for ; numberCopy != 0; {
+				r := numberCopy % 10
+				numberCopy /= 10
+				number2 = number2 * 10 + r
+			}
 			newline = append(newline, int64(number))
 		}
 		lines = append(lines, newline)
-		
 	}
+
 	var result1 int64
+	var result2 int64
 
 	for i := 0; i < len(operations); i++ {
 		temp := int64(0)
@@ -56,5 +74,9 @@ func main() {
 		}
 		result1 += temp
 	}
+
+
+
 	fmt.Println("Day 06 Part 01 :", result1)
+	fmt.Println("Day 06 Part 02 :", result2)
 }
