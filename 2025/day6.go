c0b158903c6199de8e23294540271db5ab7987b0
samsepi0x0 <64140687+samsepi0x0@users.noreply.github.com>
2026-08-19 02:13:55 +0530

    Update day6.go Part 02
    Grid approach to solve for part 2

--- 2025/day6.go
+++ 2025/day6.go
@@ -24,6 +24,7 @@ func main() {
 
 	var lines [][]int64
 	var operations []string
+	var grid [][]int
 	
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -35,6 +36,90 @@ func main() {
 					operations = append(operations, numbers[i])
 				}
 			}
+			continue
+		}
+
+		var row []int
+
+		for i := 0; i < len(numbers); i++ {
+			for j := 0; j < len(numbers[i]); j++ {
+				row = append(row, int(numbers[i][j] - '0'))
+			}
+			row = append(row, 11)
+		}
+
+		grid = append(grid, row)
+
+		var newline []int64
+		for i := 0; i < len(numbers); i++ {
+			if numbers[i] == "" {
+				continue
+			}
+			number, _ := strconv.Atoi(numbers[i])
+			number2 := 0
+			numberCopy := number
+
+			for ; numberCopy != 0; {
+				r := numberCopy % 10
+				numberCopy /= 10
+				number2 = number2 * 10 + r
+			}
+			newline = append(newline, int64(number))
+		}
+		lines = append(lines, newline)
+	}
+
+	var result1 int64
+	var result2 int64
+	startCol := 0
+
+	for i := 0; i < len(operations); i++ {
+		temp, temp2 := int64(0), int64(0)
+		if operations[i] == "*" {
+			temp = int64(1)
+			temp2 = int64(1)
+		}
+		// Part 1
+		for j := 0; j < len(lines); j++ {
+			if operations[i] == "+" {
+				temp = temp + lines[j][i]
+			} else {
+				temp = temp * lines[j][i]
+			}
+		}
+		result1 += temp
+
+		// Part 2
+		elevens := 0
+		for ;; {
+			value := int64(0)
+			for k := 0; k < len(grid); k++ {
+				if grid[k][startCol] == 11 {
+					elevens++
+					continue
+				} else {
+					value = value * 10 + int64(grid[k][startCol])
+				}
+			}
+
+			startCol++
+			if elevens == len(grid) {
+				break
+			}
+			
+			elevens = 0
+			if operations[i] == "*" {
+				temp2 = temp2 * value
+			} else {
+				temp2 = temp2 + value
+			}
+		}
+		result2 += temp2
+	}
+
+	fmt.Println("Day 06 Part 01 :", result1)
+	fmt.Println("Day 06 Part 02 :", result2)
+}			}
 			continue
 		}
 
